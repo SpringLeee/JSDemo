b486d6a1ac744410df7561088bc58a1d6a4cbe11
SpringLee <1028789852@qq.com>
2017-07-24 14:29:24 +0800

    submit

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-box = "hehe";
-box = 100;
-console.log(box)
+﻿//box = "hehe";
+//box = 100;
+//console.log(box)
+
+
+//var d = new Date()
+//console.log(d)
+//console.log("天----" + d.getDate())
+//console.log("星期几" + d.getDay())
+//console.log("小时---" + d.getHours())
+//console.log("月" + d.getMonth())
+//console.log("分" + d.getSeconds())
+
+
+
+//var t = {}
+//t.a = "aaa";
+//t.b = "bbbb";
+//console.log(t.a)
+
+
+
+
